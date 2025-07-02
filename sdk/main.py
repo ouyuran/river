20edfade012b665f023598dda4e63a7fb3e16d63
Yuran Ou <hello@ouyuran.com>
2025-07-02 23:41:44 +0800

    Support upstream job and a parameter of downstream job

--- sdk/main.py
+++ sdk/main.py
@@ -2,13 +2,22 @@ from .job import Job
 from .task import ShellTask
 
 def main():
-    def test_job_main(job):
-        job.run_task(ShellTask("echo 'Hello, World!'"))
 
-    job1 = Job("test", test_job_main)
-    job2 = Job("test2", test_job_main)
-    job2.join([job1])
-    job1.join([job2])
+    init_job = Job("my_init_job", lambda: print("init_job"))
+
+    job1 = Job(
+        "my_first_job",
+        lambda: ShellTask("echo 'Hello, World!'").execute(),
+        upstreams=[init_job]
+    )
+    job2 = Job(
+        "my_final_job", 
+        main = lambda my_first_job1, my_second_job: print(my_first_job1.name, my_first_job1._upstreams),
+        upstreams=[
+            job1,
+            Job("my_second_job", lambda: ShellTask("echo 'Hello, World!'").execute())
+        ]
+    )
 
     job2.run()
 
