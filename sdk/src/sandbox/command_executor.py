785754ad121c17145e83e80f2eb53ecb2c8f5950
Yuran Ou <yuran.ou@citrix.com>
2025-07-19 11:34:29 +0800

    Fix typo bug

--- sdk/src/sandbox/command_executor.py
+++ sdk/src/sandbox/command_executor.py
@@ -24,7 +24,7 @@ class LocalCommandExecutor(CommandExecutor):
     """Local command executor"""
 
     def run(
-        self, command: str, cwd: str = ".d", env: Optional[dict[str, str]] = None
+        self, command: str, cwd: str = ".", env: Optional[dict[str, str]] = None
     ) -> Result:
         with Connection("localhost") as connection, connection.cd(cwd):
             result = connection.local(command, env=env or {}, hide=True, warn=True)
